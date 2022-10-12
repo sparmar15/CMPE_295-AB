dd4783d15bdcf1028e7126f706076ef8306a9758
Rohit Yadav <89368283+Rohitky34@users.noreply.github.com>
2022-10-11 19:52:53 -0700

    Add home page

---
+++ code/frontend/car-pooling-app/src/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Peer-to-Peer Car Pooling
+      </header>
+    </div>
+  );
+}
+
+export default Home;
