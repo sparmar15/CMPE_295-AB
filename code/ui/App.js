bf9ac80cb90ea92bebf897c75476b6b25881847a
Soham Kasar <soham@Sohams-MacBook-Air.local>
2023-02-24 16:10:14 -0800

    folder changes

---
+++ code/ui/App.js
@@ -0,0 +1,24 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+      {/* <Image
+          style={{width: '100%', height: '50%'}}
+          source={require('./../..images/295A Project Architecture.png')}
+      /> */}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
