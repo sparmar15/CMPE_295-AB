4c77dbe11c6329c450b5939ffa96117975a510a9
Soham Kasar <soham@Sohams-MacBook-Air.local>
2023-03-08 01:11:06 -0800

    add web3auth

--- UI/Screens/LoginScreen.js
+++ UI/Screens/LoginScreen.js
@@ -10,8 +10,76 @@ import {
   ImageBackground,
 } from 'react-native';
 import styles from '../Styles/LoginScreen';
+import * as WebBrowser from '@toruslabs/react-native-web-browser';
+import Web3Auth, {
+  LOGIN_PROVIDER,
+  OPENLOGIN_NETWORK,
+} from '@web3auth/react-native-sdk';
+import RPC from '../../ethersRPC'; // for using ethers.js
+
+const scheme = 'carpool://auth'; // Or your desired app redirection scheme
+const resolvedRedirectUrl = `${scheme}://openlogin`;
+const clientId =
+  'BG9JhziXn3TewAdwRyUyinmvLCmC8z8h_0TIWQ25C-PSXd875e_ocLjhk8eb10-JRQZ7mVOCASXN4WLBAjo8BAY';
 
 const LoginScreen = ({navigation}) => {
+  const [key, setKey] = useState('');
+  const [userInfo, setUserInfo] = useState('');
+  const [console, setConsole] = useState('');
+
+  const login = async () => {
+    try {
+      setConsole('Logging in');
+      const web3auth = new Web3Auth(WebBrowser, {
+        clientId,
+        network: OPENLOGIN_NETWORK.CYAN, // or other networks
+      });
+      const info = await web3auth.login({
+        loginProvider: LOGIN_PROVIDER.GOOGLE,
+        redirectUrl: resolvedRedirectUrl,
+        mfaLevel: 'default',
+        curve: 'secp256k1',
+      });
+
+      setUserInfo(info);
+      setKey(info.privKey);
+      uiConsole('Logged In');
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  // const getChainId = async () => {
+  //   setConsole('Getting chain id');
+  //   const networkDetails = await RPC.getChainId();
+  //   uiConsole(networkDetails);
+  // };
+
+  // const getAccounts = async () => {
+  //   setConsole('Getting account');
+  //   const address = await RPC.getAccounts(key);
+  //   uiConsole(address);
+  // };
+  // const getBalance = async () => {
+  //   setConsole('Fetching balance');
+  //   const balance = await RPC.getBalance(key);
+  //   uiConsole(balance);
+  // };
+  // const sendTransaction = async () => {
+  //   setConsole('Sending transaction');
+  //   const tx = await RPC.sendTransaction(key);
+  //   uiConsole(tx);
+  // };
+  // const signMessage = async () => {
+  //   setConsole('Signing message');
+  //   const message = await RPC.signMessage(key);
+  //   uiConsole(message);
+  // };
+
+  // const uiConsole = (...args) => {
+  //   setConsole(JSON.stringify(args || {}, null, 2) + '\n\n\n\n' + console);
+  // };
+
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [usernameError, setUsernameError] = useState('');
@@ -98,7 +166,7 @@ const LoginScreen = ({navigation}) => {
             title="Submit"
             accessibilityLabel="Signin to your account"
           />
-          <Pressable onPress={handleSignupPress}>
+          <Pressable onPress={login}>
             <Text style={styles.desc}>Not a member yet? Signup here!</Text>
           </Pressable>
         </View>
