863adbac18cca1cd6a05dd2ae8bd73731d59da75
Siddhant Satish Parmar <siddhantsatish.parmar@sjsu.edu>
2023-03-27 22:17:59 -0700

    Minor changes in Driver and Rider routes

--- Backend/Routes/driver.js
+++ Backend/Routes/driver.js
@@ -10,21 +10,24 @@ const driverRoute = express.Router();
 
 // Define endpoints for Rider document
 driverRoute.post('/addDriver', async (req, res) => {
-  // const options = {
-  //   pinataMetadata: {
-  //     name: 'MyCustomName',
-  //     keyvalues: {
-  //       _id: '008',
-  //       user_name: 'siodasf',
-  //     },
-  //   },
-  //   pinataOptions: {
-  //     cidVersion: 0,
-  //     wrapWithDirectory: true,
-  //   },
-  // };
+  const val1 = req.body.data._id;
+  const val2 = req.body.data.username;
+
+  const options = {
+    pinataMetadata: {
+      name: val1,
+      keyvalues: {
+        user_name: val2,
+      },
+    },
+    pinataOptions: {
+      cidVersion: 0,
+      wrapWithDirectory: true,
+    },
+  };
   try {
-    const result = await addDriver(req.body.data, req.body.options);
+    const result = await addDriver(req.body.data, options);
+    req.session.driverHash = result.IpfsHash;
     return res.status(200).send({
       success: true,
       message: 'Driver added successfully',
@@ -42,8 +45,8 @@ driverRoute.post('/addDriver', async (req, res) => {
 
 driverRoute.get('/getDriver', async (req, res) => {
   try {
-    // const cid = 'Qmc9RYapT1qUvHerpZqpaLQsmucW7Evm3tM8ZrypvGMxX7';
-    const result = await getDriverByHash(req.query.cid);
+    const cid = req.session.driverHash;
+    const result = await getDriverByHash(cid);
     return res.status(200).send({
       success: true,
       message: 'Driver data retrieved successfully',
@@ -60,23 +63,22 @@ driverRoute.get('/getDriver', async (req, res) => {
 });
 
 driverRoute.get('/getFileHash', async (req, res) => {
-  // const filters = {
-  //   metadata: {
-  //     name: 'MyCustomName',
-  //     keyvalues: {
-  //       _id: {
-  //         value: '008',
-  //         op: 'eq',
-  //       },
-  //       user_name: {
-  //         value: 'siodasf',
-  //         op: 'eq',
-  //       },
-  //     },
-  //   },
-  // };
+  const val1 = req.body.data._id;
+  const val2 = req.body.data.username;
+  const filters = {
+    metadata: {
+      name: val1,
+      keyvalues: {
+        user_name: {
+          value: val2,
+          op: 'eq',
+        },
+      },
+    },
+  };
   try {
-    const result = await getFileHash(req.body.filters);
+    const result = await getFileHash(filters);
+    req.session.driverHash = result;
     return res.status(200).send({
       success: true,
       message: 'File hash retrieved successfully',
--- Backend/Routes/rider.js
+++ Backend/Routes/rider.js
@@ -10,8 +10,24 @@ const riderRoute = express.Router();
 
 // Define endpoints for Rider document
 riderRoute.post('/addRider', async (req, res) => {
+  const val1 = req.body.data._id;
+  const val2 = req.body.data.username;
+
+  const options = {
+    pinataMetadata: {
+      name: val1,
+      keyvalues: {
+        user_name: val2,
+      },
+    },
+    pinataOptions: {
+      cidVersion: 0,
+      wrapWithDirectory: true,
+    },
+  };
   try {
-    const result = await addRider(req.body.data, req.body.options);
+    const result = await addRider(req.body.data, options);
+    req.session.riderHash = result.IpfsHash;
     return res.status(200).send({
       success: true,
       message: 'Rider added successfully',
@@ -29,7 +45,8 @@ riderRoute.post('/addRider', async (req, res) => {
 
 riderRoute.get('/getRider', async (req, res) => {
   try {
-    const result = await getRiderByHash(req.query.cid);
+    const cid = req.session.riderHash;
+    const result = await getRiderByHash(cid);
     return res.status(200).send({
       success: true,
       message: 'Rider data retrieved successfully',
@@ -46,8 +63,22 @@ riderRoute.get('/getRider', async (req, res) => {
 });
 
 riderRoute.get('/getFileHash', async (req, res) => {
+  const val1 = req.body.data._id;
+  const val2 = req.body.data.username;
+  const filters = {
+    metadata: {
+      name: val1,
+      keyvalues: {
+        user_name: {
+          value: val2,
+          op: 'eq',
+        },
+      },
+    },
+  };
   try {
-    const result = await getFileHash(req.body.filters);
+    const result = await getFileHash(filters);
+    req.session.riderHash = result;
     return res.status(200).send({
       success: true,
       message: 'File hash retrieved successfully',
