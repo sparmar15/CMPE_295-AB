01b4fb8e4a871bf92d338beb6fb2f7baf665aaad
Siddhant Satish Parmar <siddhantsatish.parmar@sjsu.edu>
2023-04-05 16:58:48 -0700

    Setup getBalance and ethers.js

--- UI/Screens/LoginScreens/LoginScreen.js
+++ UI/Screens/LoginScreens/LoginScreen.js
@@ -3,52 +3,33 @@ import {Image, View, Text, TouchableOpacity} from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import {logger} from 'react-native-logs';
 import styles from '../../Styles/LoginScreens/LoginScreen';
-import * as WebBrowser from '@toruslabs/react-native-web-browser';
-import Web3Auth, {
-  LOGIN_PROVIDER,
-  OPENLOGIN_NETWORK,
-  LoginProvider,
-} from '@web3auth/react-native-sdk';
 import {useDispatch} from 'react-redux';
 import {userLogin} from '../../../Redux/Actions/UserActions';
 import {store} from '../../../Redux/store';
-
-import RPC from '../../ethersRPC'; // for using ethers.js
-const scheme = 'carpool://auth'; // Or your desired app redirection scheme
-const resolvedRedirectUrl = `${scheme}://openlogin`;
-const clientId =
-  'BLOwyDDYwHFKGpp1ffvnfz3IY9_rvTBbwH7U__xudIgauSPzG7NadwFVspA4-PIgIUcwIDDL_3QBEUUp-X3oBl8';
+import web3Auth from '../../../Utils/web3Auth';
 
 const LoginScreen = ({navigation}) => {
-  const [userLoginInfo, setUserLoginIInfo] = useState(null);
-  const [isUserLoggedIn, setIsUserLoggedIn] = useState(false);
+  // const [key, setKey] = useState('');
+  const [userInfo, setUserInfo] = useState('');
+  // const [console, setConsole] = useState('');
   const Log = logger.createLogger();
-  const dispatch = useDispatch();
+  // const [isLoggedIn, setIsLoggedIn] = useState(false);
+  // const dispatch = useDispatch();
+  const {login, getBalance} = web3Auth();
 
-  const login = async () => {
-    try {
-      const web3auth = new Web3Auth(WebBrowser, {
-        clientId,
-        network: OPENLOGIN_NETWORK.TESTNET, // or other networks
-      });
-      const info = await web3auth.login({
-        loginProvider: LOGIN_PROVIDER.GOOGLE,
-        redirectUrl: resolvedRedirectUrl,
-        mfaLevel: 'default',
-        curve: 'secp256k1',
-      });
-      setUserLoginIInfo(info.userInfo);
-      if (userLoginInfo) {
-        dispatch(userLogin(userLoginInfo));
-        setIsUserLoggedIn(true);
-      }
-    } catch (e) {
-      Log.info(e);
-    }
+  const handleLogin = async () => {
+    const info = await login();
+    setUserInfo(info);
+    Log.info(info);
+    // navigation.navigate('TabNavigator');
   };
-  if (isUserLoggedIn) {
-    navigation.navigate('LandingPage');
-  }
+
+  const handleSignupPress = async () => {
+    const info = await getBalance();
+    // navigation.navigate('DriverRiderSelect');
+    // dispatch(userLogin({name: 'sid', pass: '123'}));
+    Log.info(info);
+  };
 
   return (
     <View style={styles.container}>
@@ -65,7 +46,7 @@ const LoginScreen = ({navigation}) => {
       <Text style={styles.descriptionText}>
         To the easiest way to share and earn Karma!
       </Text>
-      <TouchableOpacity style={styles.googleSignInButton} onPress={login}>
+      <TouchableOpacity style={styles.googleSignInButton} onPress={handleLogin}>
         <Icon name="google" size={24} color="#fff" />
         <Text style={styles.googleSignInButtonText}>Login with Google</Text>
       </TouchableOpacity>
